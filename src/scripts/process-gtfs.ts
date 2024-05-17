60511b8a5d0dd345b03d07a75288f86e7e21760a
Laurentiu Pricop <laurcons@outlook.com>
2024-05-17 10:00:12 +0300

    add note to script

--- src/scripts/process-gtfs.ts
+++ src/scripts/process-gtfs.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { GtfsModule } from 'src/modules/gtfs/gtfs.module';
 import { GtfsService } from 'src/modules/gtfs/gtfs.service';
 
+// npx tsx src/scripts/process-gtfs.ts
+
 async function run() {
   const app = await NestFactory.createApplicationContext(GtfsModule);
   const gtfs = app.get(GtfsService);
